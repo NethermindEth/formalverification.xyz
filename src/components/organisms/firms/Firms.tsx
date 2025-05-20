3f469a596f960334fba66979a4f199f6e59d09c1
Prince Anuragi <princanurag07@gmail.com>
2025-05-20 19:29:25 +0530

    feat: new re-designed website (#5)
    * feat: init project + components
    
    * fix: update filters and added footer
    
    * fix: update no firms view
    
    * feat: add about page
    
    * fix: refactor and mobile nav addition
    
    * feat: cleanup and fix final website
    
    * feat: redesign initial final
    
    * fix: package.json update
    
    * fix: scripts addition
    
    * fix: update script
    
    * fix: ts

---
+++ src/components/organisms/firms/Firms.tsx
@@ -0,0 +1,112 @@
+import { useMemo, useReducer } from 'react'
+
+import "./firms.css";
+import { Flex, Heading, Text } from '@radix-ui/themes';
+import Menu from './Menu';
+
+import { firms, FVTools, fvTools, Language, language, Speciality, speciality, Stack, stack } from "../../../data/firms";
+import Firm from './Firm';
+
+export type Action = {
+	type: "SPECIALITIES" | "FVTOOLS" | "STACKS" | "LANGUAGES",
+	newState: string[]
+}
+
+type State = {
+	"SPECIALITIES" : Speciality[],
+	"FVTOOLS" : FVTools[],
+	"STACKS" : Stack[],
+	"LANGUAGES": Language[]
+}
+
+function reducer(state: State, action: Action) {
+	return {
+		...state,
+		[action.type]: action.newState
+	}
+}
+
+const Firms = () => {
+
+	const [filterState, dispatchFilter] = useReducer(reducer, {
+		"SPECIALITIES": speciality.options,
+		"FVTOOLS": fvTools.options,
+		"STACKS": stack.options,
+		"LANGUAGES": language.options
+	})
+
+	const filteredFirms = useMemo(() => {
+		return firms.filter(firm => {
+			const sState = filterState.SPECIALITIES.filter((speciality) => firm.specialities.includes(speciality))
+			const fState = filterState.FVTOOLS.filter((fvTool) => firm.fv_language.includes(fvTool))
+			const stackState = filterState.STACKS.filter((stack) => firm.stacks.includes(stack))
+			const lState = filterState.LANGUAGES.filter((language) => firm.languages.includes(language))
+			return sState?.length > 0 && fState.length > 0 && stackState.length > 0 && lState.length > 0
+		})
+		}, [filterState])
+
+	return (
+		<Flex direction={"column"} gap={"4"} pb={"9"}>	
+			<Heading size={{ initial: "5", sm: "6" }} align={"center"}>Choose the firm that meets your needs!</Heading>
+			<Flex 
+				direction={{
+					initial: "column", 
+					sm: "row"
+				}} 
+				gap={{initial: "3", sm: "0"}}
+				justify={"between"} 
+				align={"stretch"}
+			>
+				<Menu 
+					options={speciality.options} 
+					dispatcher={dispatchFilter} 
+					label='Specialities' 
+					actionType='SPECIALITIES'
+				/>
+				<Menu 
+					options={fvTools.options} 
+					dispatcher={dispatchFilter} 
+					label='Formal Verification Tools' 
+					actionType='FVTOOLS'
+				/>
+				<Menu 
+					options={stack.options} 
+					dispatcher={dispatchFilter}
+					label='Stacks' 
+					actionType='STACKS'
+				/>
+				<Menu 
+					options={language.options} 
+					dispatcher={dispatchFilter} 
+					label='Languages' 
+					actionType='LANGUAGES'
+				/>
+			</Flex>
+			<Flex py={"4"} px={{ initial: "2", sm: "0" }} gap={{ initial: "3", sm: "5" }} direction={"column"}>
+				{ 
+					filteredFirms?.length > 0 
+						? <>
+							<Flex px={"2"}>
+								<Text>{filteredFirms.length} FIRMS FOUND</Text>
+							</Flex>
+							{ 
+								filteredFirms.map((firm) => {
+									return <Firm key={JSON.stringify(firm)} firm={firm} />
+								})
+							}
+						</> 
+						: <Flex direction={"column"} align={"center"} gap={"2"} justify={"center"} px={{ initial: "2", sm: "9" }} py={{ initial: "7", sm: "9" }} className='no-firm-wrapper'>
+							<img src='exclaimation.svg' alt='exclaimation' />
+							<Text align={"center"}>
+								No firms found with the selected filters.
+								<br />
+								Please try another combination.
+							</Text>
+						</Flex> 
+				}
+			</Flex>
+		</Flex>
+	)
+}
+
+export default Firms
--- src/data/firms.ts
+++ src/data/firms.ts
@@ -1,228 +1,310 @@
-export type Firm = {
-  id: number;
-  name: string;
-  description: string;
-  specialties: Speciality[];
-  fv_language: FVTools[];
-  stacks: Stack[];
-  languages: Language[];
-  svg: string;
-  link: string;
-};
+import { z } from "zod";
 
-export enum Stack {
-  Ethereum = "Ethereum",
-  BinanceSmartChain = "Binance Smart Chain",
-  Solana = "Solana",
-  Tezos = "Tezos",
-  Algorand = "Algorand",
-  Polygon = "Polygon",
-  Starknet = "Starknet",
-  AlephZero = "Aleph Zero",
-  Zcash = "Zcash",
-  Cosmos = "Cosmos",
-  TON = "TON",
-  zkWasm = "zkWasm",
-  Stellar = "Stellar",
-  Cardano = "Cardano",
-  Polkadot = "Polkadot",
-  MultiversX = "MultiversX"
-}
+export const stack = z.enum([
+ "Ethereum",
+ "Binance Smart Chain",
+ "Solana",
+ "Tezos",
+ "Algorand",
+ "Polygon",
+ "Starknet",
+ "Aleph Zero",
+  "Zcash",
+  "Cosmos",
+  "TON",
+  "zkWasm",
+  "Stellar",
+  "Cardano",
+  "Polkadot",
+  "MultiversX"
+])
 
-export enum FVTools {
-  Coq = "Coq",
-  SMTSolvers = "SMT Solvers (Z3, CVC4)",
-  HoareLogic = "Hoare Logic",
-  KFramework = "K Framework",
-  Z3 = "Z3",
-  TLAPlus = "TLA+",
-  IsabelleHOL = "Isabelle/HOL",
-  HOL4 = "HOL4",
-  Lean4 = "Lean 4",
-  CVC5 = "CVC5",
-  EasyCrypt = "EasyCrypt",
-  ACL2 = "ACL2",
-  Dafny = "Dafny",
-  Quint = "Quint",
-  SAW = "SAW"
-}
+export type Stack = z.infer<typeof stack>;
 
-export enum Speciality {
-  SmartContractVerification = "Smart Contract Verification",
-  ZKCircuitVerification = "Zero-Knowledge Circuit Verification",
-  ConsensusProtocolVerification = "Consensus Protocol Verification",
-  CryptographicPrimitiveVerification = "Cryptographic Primitive Verification",
-  VirtualMachineVerification = "Virtual Machine Verification",
-  TokenomicsVerification = "Tokenomics Model Verification",
-  CrossChainProtocolVerification = "Cross-Chain Protocol Verification",
-  PrivacyPreservingComputationVerification = "Privacy-Preserving Computation Verification",
-  OnChainGovernanceVerification = "On-Chain Governance Mechanism Verification",
-  OracleSystemVerification = "Oracle System Verification",
-  DeFiProtocolVerification = "DeFi Protocol Verification"
-}
+// export enum Stack {
+//   Ethereum = "Ethereum",
+//   BinanceSmartChain = "Binance Smart Chain",
+//   Solana = "Solana",
+//   Tezos = "Tezos",
+//   Algorand = "Algorand",
+//   Polygon = "Polygon",
+//   Starknet = "Starknet",
+//   AlephZero = "Aleph Zero",
+//   Zcash = "Zcash",
+//   Cosmos = "Cosmos",
+//   TON = "TON",
+//   zkWasm = "zkWasm",
+//   Stellar = "Stellar",
+//   Cardano = "Cardano",
+//   Polkadot = "Polkadot",
+//   MultiversX = "MultiversX"
+// }
 
-export enum Language {
-  Solidity = "Solidity",
-  Vyper = "Vyper",
-  Move = "Move",
-  DeepSEA = "DeepSEA",
-  Haskell = "Haskell",
-  Rust = "Rust",
-  Cairo = "Cairo",
-  PyTeal = "PyTeal",
-  OCaml = "OCaml",
-  TypeScript = "TypeScript",
-  Java = "Java",
-  C = "C",
-  Cryptol = "Cryptol",
-  Soroban = "Soroban",
-  TEAL = "TEAL",
-  Go = "Go"
-}
+// export enum FVTools {
+//   Coq = "Coq",
+//   SMTSolvers = "SMT Solvers (Z3, CVC4)",
+//   HoareLogic = "Hoare Logic",
+//   KFramework = "K Framework",
+//   Z3 = "Z3",
+//   TLAPlus = "TLA+",
+//   IsabelleHOL = "Isabelle/HOL",
+//   HOL4 = "HOL4",
+//   Lean4 = "Lean 4",
+//   CVC5 = "CVC5",
+//   EasyCrypt = "EasyCrypt",
+//   ACL2 = "ACL2",
+//   Dafny = "Dafny",
+//   Quint = "Quint",
+//   SAW = "SAW"
+// }
 
+export const fvTools = z.enum([
+  "Coq",
+  "SMT Solvers (Z3, CVC4)",
+  "Hoare Logic",
+  "K Framework",
+  "Z3",
+  "TLA+",
+  "Isabelle/HOL",
+  "HOL4",
+  "Lean 4",
+  "CVC5",
+  "EasyCrypt",
+  "ACL2",
+  "Dafny",
+  "Quint",
+  "SAW"
+])
+
+export type FVTools = z.infer<typeof fvTools>
+
+// export enum Language {
+//   Solidity = "Solidity",
+//   Vyper = "Vyper",
+//   Move = "Move",
+//   DeepSEA = "DeepSEA",
+//   Haskell = "Haskell",
+//   Rust = "Rust",
+//   Cairo = "Cairo",
+//   PyTeal = "PyTeal",
+//   OCaml = "OCaml",
+//   TypeScript = "TypeScript",
+//   Java = "Java",
+//   C = "C",
+//   Cryptol = "Cryptol",
+//   Soroban = "Soroban",
+//   TEAL = "TEAL",
+//   Go = "Go"
+// }
+
+export const speciality = z.enum([
+ "Smart Contract Verification",
+ "Zero-Knowledge Circuit Verification",
+ "Consensus Protocol Verification",
+ "Cryptographic Primitive Verification",
+ "Virtual Machine Verification",
+ "Tokenomics Model Verification",
+ "Cross-Chain Protocol Verification",
+ "Privacy-Preserving Computation Verification",
+ "On-Chain Governance Mechanism Verification",
+ "Oracle System Verification",
+ "DeFi Protocol Verification"
+])
+
+export type Speciality = z.infer<typeof speciality>
+
+export const language = z.enum([
+ "Solidity",
+ "Vyper",
+ "Move",
+ "DeepSEA",
+ "Haskell",
+ "Rust",
+ "Cairo",
+ "PyTeal",
+ "OCaml",
+  "TypeScript",
+  "Java",
+  "C",
+  "Cryptol",
+  "Soroban",
+  "TEAL",
+  "Go",
+  "Circom"
+])
+
+export type Language = z.infer<typeof language>
+
+export const firm = z.object({
+  id: z.number(),
+  name: z.string(),
+  description: z.string(),
+  specialities: z.array(speciality),
+  fv_language: z.array(fvTools),
+  stacks: z.array(stack),
+  languages: z.array(language),
+  svg: z.string(),
+  png: z.string(),
+  link: z.string(),
+})
+
+export type Firm = z.infer<typeof firm>
 
 export const firms: Firm[] = [
     {
       "id": 1,
       "name": "Verifereum",
-      "specialities": [Speciality.SmartContractVerification, Speciality.DefiProtocolVerification, Speciality.VirtualMachineVerification],
+      "specialities": ["Smart Contract Verification", "DeFi Protocol Verification", "Virtual Machine Verification"],
       "description": "Free and open source project developing tools and methods for full formal verification of EVM applications in higher-order logic (using HOL4).",
-      "languages": [Language.Vyper, Language.Solidity],
-      "stacks": [Stack.Ethereum],
-      "fv_language": [FVTools.HOL4, FVTools.HoareLogic],
+      "languages": ["Vyper", "Solidity"],
+      "stacks": ["Ethereum"],
+      "fv_language": ["HOL4", "Hoare Logic"],
       "svg": "verifereum.svg",
+      "png": "verifereum.png",
       "link": "https://verifereum.org"
     },
     {
       "id": 2,
       "name": "ConsenSys Diligence",
-      "specialties": [Speciality.SmartContractVerification, Speciality.DeFiProtocolVerification],
+      "specialities": ["Smart Contract Verification", "DeFi Protocol Verification"],
       "description": "As part of ConsenSys, Diligence offers advanced security services for Ethereum-based projects, including manual audits and automated analysis with tools like MythX.",
-      "languages": [Language.Solidity],
-      "stacks": [Stack.Ethereum],
-      "fv_language": [FVTools.HoareLogic, FVTools.Dafny],
+      "languages": ["Solidity"],
+      "stacks": ["Ethereum"],
+      "fv_language": ["Hoare Logic", "Dafny"],
       "svg": "consensys.svg",
+      "png": "consensys.png",
       "link": "https://consensys.net/diligence"
     },
     {
       "id": 3,
       "name": "Runtime Verification",
-      "specialties": [Speciality.SmartContractVerification, Speciality.DeFiProtocolVerification, Speciality.VirtualMachineVerification, Speciality.ConsensusProtocolVerification],
+      "specialities": ["Smart Contract Verification", "DeFi Protocol Verification", "Virtual Machine Verification", "Consensus Protocol Verification"],
       "description": "Focused on formal verification techniques, they provide comprehensive smart contract verification, fuzz testing, security auditing, and advisory services for various blockchain ecosystems, while also developing high-quality open-source security tools.",
-      "languages": [Language.Solidity, Language.Vyper, Language.Rust, Language.Soroban, Language.Haskell, Language.TEAL, Language.PyTeal, Language.Go],
-      "stacks": [Stack.Ethereum, Stack.Stellar, Stack.Polkadot, Stack.MultiversX, Stack.Cosmos, Stack.Tezos, Stack.Algorand],
-      "fv_language": [FVTools.KFramework, FVTools.Z3, FVTools.Lean4, FVTools.Coq],
+      "languages": ["Solidity", "Vyper", "Rust", "Soroban", "Haskell", "TEAL", "PyTeal", "Go"],
+      "stacks": ["Ethereum", "Stellar", "Polkadot", "MultiversX", "Cosmos", "Tezos", "Algorand"],
+      "fv_language": ["K Framework", "Z3", "Lean 4", "Coq"],
       "svg": "runtime.svg",
+      "png": "runtime.png",
       "link": "https://runtimeverification.com"
     },
     {
       "id": 4,
       "name": "Veridise",
-      "specialties": [Speciality.ZKCircuitVerification, Speciality.SmartContractVerification],
+      "specialities": ["Zero-Knowledge Circuit Verification", "Smart Contract Verification"],
       "description": "Veridise is known for its rigorous security audits and formal verification of blockchain systems, leveraging state-of-the-art research tools.",
-      "languages": [Language.Solidity, Language.Rust, Language.Cairo],
-      "stacks": [Stack.Ethereum, Stack.Solana, Stack.Starknet],
-      "fv_language": [FVTools.Coq, FVTools.Z3],
+      "languages": ["Solidity", "Rust", "Cairo"],
+      "stacks": ["Ethereum", "Solana", "Starknet"],
+      "fv_language": ["Coq", "Z3"],
       "svg": "veridise.svg",
-      "link": "https://www.veridise.com"
+      "png": "veridise.png",
+      "link": "https://www.veridise.com/"
     },
     {
       "id": 5,
       "name": "Cyberscope",
-      "specialties": [Speciality.SmartContractVerification, Speciality.DeFiProtocolVerification],
+      "specialities": ["Smart Contract Verification", "DeFi Protocol Verification"],
       "description": "Cyberscope specializes in formal verification, offering tailored solutions for smart contract projects, ensuring rigorous mathematical proof of correctness and security.",
-      "languages": [Language.Solidity, Language.Vyper],
-      "stacks": [Stack.Ethereum, Stack.BinanceSmartChain],
-      "fv_language": [FVTools.Coq, FVTools.TLAPlus],
+      "languages": ["Solidity", "Vyper"],
+      "stacks": ["Ethereum", "Binance Smart Chain"],
+      "fv_language": ["Coq", "TLA+"],
       "svg": "cyberscope.svg",
-      "link": "https://www.cyberscope.io"
+      "png": "cyberscope.png",
+      "link": "https://www.cyberscope.io/"
     },
     {
       "id": 6,
       "name": "ShellBoxes",
-      "specialties": [Speciality.SmartContractVerification, Speciality.CryptographicPrimitiveVerification],
+      "specialities": ["Smart Contract Verification", "Cryptographic Primitive Verification"],
       "description": "ShellBoxes offers comprehensive formal verification and security audits for various blockchain ecosystems, providing detailed reports and mitigation strategies.",
-      "languages": [Language.Solidity, Language.Rust, Language.PyTeal],
-      "stacks": [Stack.Ethereum, Stack.Polygon, Stack.Algorand],
-      "fv_language": [FVTools.IsabelleHOL, FVTools.TLAPlus],
+      "languages": ["Solidity", "Rust", "PyTeal"],
+      "stacks": ["Ethereum", "Polygon", "Algorand"],
+      "fv_language": ["Isabelle/HOL", "TLA+"],
       "svg": "shellboxes.svg",
-      "link": "https://www.shellboxes.com"
+      "png": "shellboxes.png",
+      "link": "https://www.shellboxes.com/"
     },
     {
       "id": 7,
       "name": "Nethermind",
-      "specialties": [Speciality.SmartContractVerification, Speciality.ZKCircuitVerification, Speciality.CryptographicPrimitiveVerification, Speciality.ConsensusProtocolVerification],
+      "specialities": ["Smart Contract Verification", "Zero-Knowledge Circuit Verification", "Cryptographic Primitive Verification", "Consensus Protocol Verification"],
       "description": "Nethermind specializes in hard formal methods, leveraging the interactive theorem provers such as Lean4 and EasyCrypt to verify the most complex software in the web3 ecosystem. They focus particularly in the fields of smart-contract, zk-circuit, cryptographic protocol and distributed protocol verification.",
-      "languages": [Language.Solidity, Language.Cairo],
-      "stacks": [Stack.Ethereum, Stack.Starknet],
-      "fv_language": [FVTools.Lean4, FVTools.SMTSolvers],
+      "languages": ["Solidity", "Cairo"],
+      "stacks": ["Ethereum", "Starknet"],
+      "fv_language": ["Lean 4", "SMT Solvers (Z3, CVC4)"],
       "svg": "nethermind.svg",
+      "png": "nethermind.png",
       "link": "https://www.nethermind.io/formal-verification"
     },
     {
       "id": 8,
       "name": "Formal Land",
-      "specialties": [Speciality.SmartContractVerification, Speciality.ConsensusProtocolVerification],
+      "specialities": ["Smart Contract Verification", "Consensus Protocol Verification"],
       "description": "Formal Land offers advanced formal verification services, specializing in translating code from languages like Rust, OCaml, Solidity, and TypeScript to the Coq proof system. They are known for their rigorous methods and innovative tools, ensuring the highest level of software security.",
-      "languages": [Language.Solidity, Language.Rust, Language.OCaml, Language.TypeScript],
-      "stacks": [Stack.Ethereum, Stack.AlephZero, Stack.Tezos],
-      "fv_language": [FVTools.Coq],
+      "languages": ["Solidity", "Rust", "OCaml", "TypeScript"],
+      "stacks": ["Ethereum", "Aleph Zero", "Tezos"],
+      "fv_language": ["Coq"],
       "svg": "formalland.svg",
-      "link": "https://formal.land"
+      "png": "formalland.png",
+      "link": "https://formal.land/"
     },
     {
       "id": 9,
       "name": "Kestrel",
-      "specialties": [Speciality.ZKCircuitVerification, Speciality.VirtualMachineVerification],
+      "specialities": ["Zero-Knowledge Circuit Verification", "Virtual Machine Verification"],
       "description": "Kestrel's research spans formal methods, program synthesis and refinement, program analysis and verification, formal modeling and validation, theorem proving, and planning.",
-      "languages": [Language.Java],
-      "stacks": [Stack.Ethereum],
-      "fv_language": [FVTools.ACL2],
+      "languages": ["Java"],
+      "stacks": ["Ethereum"],
+      "fv_language": ["ACL2"],
       "svg": "kestrel.svg",
+      "png": "kestrel.png",
       "link": "https://www.kestrel.edu"
     },
     {
       "id": 10,
       "name": "CertiK",
-      "specialties": [Speciality.ZKCircuitVerification, Speciality.VirtualMachineVerification, Speciality.SmartContractVerification],
+      "specialities": ["Zero-Knowledge Circuit Verification", "Virtual Machine Verification", "Smart Contract Verification"],
       "description": "CertiK provides provides a comprehensive suite of tools to secure the Web3 industry at scale.",
       "languages": [],
-      "stacks": [Stack.Ethereum, Stack.Cosmos, Stack.zkWasm, Stack.TON],
-      "fv_language": [FVTools.Coq],
+      "stacks": ["Ethereum", "Cosmos", "zkWasm", "TON"],
+      "fv_language": ["Coq"],
       "svg": "certik.svg",
+      "png": "certik.png",
       "link": "https://www.certik.com/products/formal-verification"
     },
     {
       "id": 11,
       "name": "Galois",
-      "specialties": [Speciality.CryptographicPrimitiveVerification, Speciality.ConsensusProtocolVerification, Speciality.SmartContractVerification],
+      "specialities": ["Cryptographic Primitive Verification", "Consensus Protocol Verification", "Smart Contract Verification"],
       "description": "CertiK provides provides a comprehensive suite of tools to secure the Web3 industry at scale.",
-      "languages": [Language.C, Language.Java, Language.Cryptol],
-      "stacks": [Stack.Ethereum, Stack.Stellar, Stack.Cardano],
-      "fv_language": [FVTools.Lean4, FVTools.SAW],
+      "languages": ["C", "Java", "Cryptol"],
+      "stacks": ["Ethereum", "Stellar", "Cardano"],
+      "fv_language": ["Lean 4", "SAW"],
       "svg": "galois.svg",
+      "png": "galois.png",
       "link": "https://galois.com/project/blockchain-roundup/"
     },
     {
       "id": 12,
       "name": "zkSecurity",
-      "specialties": [Speciality.ZKCircuitVerification],
+      "specialities": ["Zero-Knowledge Circuit Verification"],
       "description": "zkSecurity is a team of experts in zero-knowledge proofs and cryptography in general. They perform security audits, development work, due diligence, research, etc.",
-      "languages": [Language.Rust, Language.Circom, Language.Solidity, Language.OCaml, Language.TypeScript, Language.Cairo, Language.Go, Language.C],
-      "stacks": [Stack.Ethereum, Stack.Starknet, Stack.Solana],
-      "fv_language": [FVTools.Lean4],
+      "languages": ["Rust", "Circom", "Solidity", "OCaml", "TypeScript", "Cairo", "Go", "C"],
+      "stacks": ["Ethereum", "Starknet", "Solana"],
+      "fv_language": ["Lean 4"],
       "svg": "zksecurity.svg",
+      "png": "zksecurity.png",
       "link": "https://zksecurity.xyz"
     },
     {
       "id": 13,
       "name": "Lindy Labs",
-      "specialties": [Speciality.SmartContractVerification],
+      "specialities": ["Smart Contract Verification"],
       "description": "Lindy Labs offers bespoke formal verification for DeFi projects running on Starknet or directly on Ethereum. We can prove your code matches what you claim to implement in your whitepaper!",
-      "languages": [Language.Cairo, Language.Solidity],
-      "stacks": [Stack.Starknet, Stack.Ethereum],
-      "fv_language": [FVTools.Lean4, FVTools.SMTSolvers],
+      "languages": ["Cairo", "Solidity"],
+      "stacks": ["Starknet", "Ethereum"],
+      "fv_language": ["Lean 4", "SMT Solvers (Z3, CVC4)"],
       "svg": "lindylabs.svg",
+      "png": "lindylabs.png",
       "link": "https://www.lindylabs.net/"
     }
   ];
